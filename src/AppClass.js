dadf7e0762b67422a0c33a40d379c3edd3f0cae8
Kurt Hakim <kurthakim@gmail.com>
2023-04-12 14:10:01 +0700

    Added classes lifecycle example

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,12 +1,82 @@
 import React, { Component, Fragment } from 'react';
 import './AppClass.css'
+import Input from "./Input"
 
 export default class AppClass extends Component {
   constructor(props) {
     super(props);
+
+    this.firstNameRef = React.createRef();
+    this.lastNameRef = React.createRef(null);
+    this.dobRef = React.createRef(null);
+
+
     this.state = {
       isTrue: false,
+      crowd: [],
     };
+  }
+
+  setFirstName(newName) {
+    this.setState({ firstName: newName });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (this.state.firstName !== "") {
+      this.addPerson(this.state.firstName, this.state.lastName, this.state.dob)
+    }
+  }
+
+  addPerson(newFirst, newLast, newDob) {
+    // create the project
+    let newPerson = {
+      id: this.state.crowd.length + 1,
+      firstName: newFirst,
+      lastName: newLast,
+      dob: newDob,
+    }
+
+    const newList = this.state.crowd.concat(newPerson);
+    const sorted = newList.sort((a, b) => {
+      if (a.lastName < b.lastName) {
+        return -1;
+      } else if (a.lastName > b.lastName) {
+        return 1;
+      }
+      return 0;
+    })
+
+
+    this.setState({ crowd: sorted })
+    this.setState({ firstName: "", lastName: "", dob: "" })
+
+    this.firstNameRef.current.value = "";
+    this.lastNameRef.current.value = "";
+    this.dobRef.current.value = "";
+  }
+
+  componentDidMount() {
+    this.setState({
+      firstName: "",
+      lastName: "",
+      dob: "",
+      crowd: [
+        {
+          id: 1,
+          firstName: "Mary",
+          lastName: "Jones",
+          dob: "1997-05-02",
+        },
+        {
+          id: 2,
+          firstName: "Jack",
+          lastName: "Smith",
+          dob: "1999-02-04",
+        },
+      ]
+    })
   }
 
   toggleTrue = () => {
@@ -21,7 +91,7 @@ export default class AppClass extends Component {
     })
   }
 
-  
+
 
   render() {
     return (
@@ -29,7 +99,7 @@ export default class AppClass extends Component {
         <hr />
         <h1 className='h1-green'>{this.props.msg}</h1>
         <hr />
-        {this.state.isTrue && 
+        {this.state.isTrue &&
           <Fragment>
             <p>The current value of isTrue is true</p>
             <hr />
@@ -37,12 +107,62 @@ export default class AppClass extends Component {
         }
         <hr />
         {this.state.isTrue
-        ? <p>Is true</p>
-        : <p>Is false</p>
+          ? <p>Is true</p>
+          : <p>Is false</p>
 
         }
         <hr />
         <a href="#!" className="btn btn-outline-secondary" onClick={this.toggleTrue}>Toggle isTrue</a>
+
+        <hr />
+        <form autoComplete='off' onSubmit={this.handleSubmit}>
+
+          <div className="mb-3">
+            <label htmlFor="first-name" className="form-label">First Name</label>
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              ref={this.firstNameRef}
+              className='form-control'
+              onChange={(event) => this.setFirstName(event.target.value)}
+            />
+          </div>
+          <Input
+            title="Last Name"
+            type="text"
+            ref={this.lastNameRef}
+            name="last-name"
+            autoComplete="last-name-new"
+            className="form-control"
+            onChange={(event) => this.setState({ lastName: event.target.value })}
+          />
+
+          <Input
+            title="Date of Birth"
+            type="date"
+            ref={this.dobRef}
+            name="dob"
+            autoComplete="dob-new"
+            className="form-control"
+            onChange={(event) => this.setState({ dob: event.target.value })}
+          />
+
+          <input type="submit" value="Submit" className='btn btn-primary' />
+        </form>
+        <div>
+          First Name: {this.state.firstName}<br />
+          Last Name: {this.state.lastName}<br />
+          DOB: {this.state.dob}<br />
+
+        </div>
+        <hr />
+        <h3>People</h3>
+        <ul className="list-group">
+          {this.state.crowd.map((m) => (
+            <li key={m.id} className='list-group-item'>{m.firstName} {m.lastName}</li>
+          ))}
+        </ul>
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import AppClass from './AppClass';
-import App from './App';
+import AppClass from './AppClass';
+// import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <div className="container">
-    <div className="row">
-      <div className="col">
-        {/* <AppClass msg="Hello, state!" /> */}
-        <App msg="State using functional components!" />
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <AppClass msg="Hello, state!" />
+          {/* <App msg="State using functional components!" /> */}
+        </div>
       </div>
     </div>
-  </div>
   </React.StrictMode>
 );
 
